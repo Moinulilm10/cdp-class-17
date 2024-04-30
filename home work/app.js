851dde01b0d5309d12362f0c9dba8c55d0d94c1a
Moinul Islam <moinulilm10@gmail.com>
2024-05-01 03:12:34 +0600

    error and response handler added in app.js

--- home work/app.js
+++ home work/app.js
@@ -2,10 +2,15 @@ const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
 const router = require("./router");
 const json = require("koa-json");
+const responseHandler = require("./handler/ResponseHandler");
+const errorHandler = require("./handler/ErrorHandler");
 
 const PORT = 8080;
 
 const app = new Koa();
+
+app.use(errorHandler());
+app.use(responseHandler());
 
 app.use(json());
 app.use(bodyParser());
